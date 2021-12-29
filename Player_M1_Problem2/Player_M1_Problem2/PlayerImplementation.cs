ef8cf13538ae408ee882dec82c26b18975eaf380
AnkithaReddymalla <reddymallaankitha@gmail.com>
2021-12-29 20:53:49 +0530

    Player Files Added

---
+++ Player_M1_Problem2/Player_M1_Problem2/PlayerImplementation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player_M1_Problem2
+{
+   
+    class PlayerImplementation
+    {
+        int listCount;
+
+        public int Average(IList<Player> players)
+        {
+            int count = 0;
+            int avg = 0;
+            foreach (var item in players)
+            {
+                avg = avg + item.Score;
+                count++;
+            }
+            avg = avg / count;
+            listCount = count;
+            return avg;
+        }
+
+        public int Max(IList<Player> players)
+        {
+            int maxScore = players[0].Score;
+            for(int i=1; i<listCount; i++ )
+            {
+                if(maxScore < players[i].Score )
+                {
+                    maxScore = players[i].Score;
+                }
+            }
+            return maxScore;
+
+        }
+
+        public int Min(IList<Player> players)
+        {
+            int minScore = players[0].Score;
+            for (int i = 1; i < listCount; i++)
+            {
+                if (minScore > players[i].Score)
+                {
+                    minScore = players[i].Score;
+                }
+            }
+            return minScore;
+
+        }
+    }
+}
---
+++ Player_M1_Problem2/Player_M1_Problem2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player_M1_Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Player> players = new List<Player>();
+            players.Add(new Player { Name = "X", Team = "A", Score = 30 });
+            players.Add(new Player { Name = "X", Team = "A", Score = 90 });
+            players.Add(new Player { Name = "X", Team = "A", Score = 50 });
+            PlayerImplementation playerImplementation = new PlayerImplementation();
+            Console.WriteLine("Average Score: "+playerImplementation.Average(players));
+            Console.WriteLine("Maximum Score: "+playerImplementation.Max(players));
+            Console.WriteLine("Minimum Score: "+playerImplementation.Min(players));
+            Console.ReadKey();
+        }
+    }
+}
